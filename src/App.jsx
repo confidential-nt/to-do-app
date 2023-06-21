7b03ebb6a489998aa93bba129eaa945a5b950fe7
confidential-nt <youyoy689@gmail.com>
2023-06-21 22:24:20 +0900

    [feat] 6.로컬 스토리지에 데이터 저장

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,48 @@ import "./App.css";
 import ItemList from "./components/item_list/ItemList";
 import ItemForm from "./components/item_form/ItemForm";
 import Header, { Filter } from "./components/header/FilterHeader";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const localStorage_key = "items";
 
 function App() {
-  const [items, setItems] = useState([
-    { id: 1231, content: "강의 보기", completed: false },
-    { id: 1233, content: "카페 가기", completed: false },
-    { id: 1234, content: "청소 하기", completed: false },
-  ]);
+  const [items, setItems] = useState([]);
   const [filter, setFilter] = useState(Filter.ALL);
 
+  useEffect(() => {
+    const storedItems = localStorage.getItem(localStorage_key);
+    if (storedItems) {
+      setItems(JSON.parse(storedItems));
+    }
+  }, []);
+
   const handleAdd = (content) => {
-    setItems((prev) => [
-      ...prev,
-      { id: Date.now(), content, completed: false },
-    ]);
+    setItems((prev) => {
+      const items = [...prev, { id: Date.now(), content, completed: false }];
+      localStorage.setItem(localStorage_key, JSON.stringify(items));
+      return items;
+    });
   };
 
   const handleDelete = (item) => {
-    setItems((prev) => prev.filter((i) => i.id !== item.id));
+    setItems((prev) => {
+      const items = prev.filter((i) => i.id !== item.id);
+      localStorage.setItem(localStorage_key, JSON.stringify(items));
+      return items;
+    });
   };
 
   const handleChangeItemState = (item) => {
-    setItems((prev) =>
-      prev.map((i) => {
+    setItems((prev) => {
+      const items = prev.map((i) => {
         if (i.id === item.id) {
           return { ...item, completed: !item.completed };
         }
         return i;
-      })
-    );
+      });
+      localStorage.setItem(localStorage_key, JSON.stringify(items));
+      return items;
+    });
   };
 
   const handleFilter = (filter) => {
