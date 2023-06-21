48e23f79743b3a896b2bd4f3bd1304a942250126
confidential-nt <youyoy689@gmail.com>
2023-06-21 18:53:28 +0900

    [feat] 1.아이템 보여주기

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import ItemList from "./components/item_list/ItemList";
+function App() {
+  return (
+    <main className="main">
+      <ItemList />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/item/Item.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Item({ title }) {
+  return (
+    <li>
+      <h2>{title}</h2>
+    </li>
+  );
+}
